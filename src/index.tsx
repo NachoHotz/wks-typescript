4e4bc3fafcee450772405c3016e57c4f10890daf
Nacho Hotz <juanhotz611@gmail.com>
2021-07-31 14:37:07 -0300

    initial client work

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-function App() {
-  return <div>Henry Workshop - Typescript</div>;
+interface AppProps {
+  title: string;
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+function App({title}: AppProps) {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h3>Henry Workshop - {title}</h3>
+      <hr/>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <br/>
+      <span>{counter}</span>
+    </div>
+  );
+}
+
+ReactDOM.render(<App title="Typescript" />, document.querySelector('#root'));
